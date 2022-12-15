70dd1bcca16d935b8c9caf49698b513fa1c91075
Mahmoud_Walid <116118315+Mahmoud-walid@users.noreply.github.com>
2022-12-16 01:45:58 +0200

    Perfect_Num
    Perfect_Num

---
+++ Perfect_Numbers/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Perfect_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Enter the start number...");
+            int srt = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the end number...");
+            int end = int.Parse(Console.ReadLine());
+
+
+
+
+            for(int i = srt; end >= i; i++)
+            {
+                int sum = 0;
+                int n = 1;
+
+                while (i > n)
+                {
+                    if(i % n == 0)
+                    
+                        sum+=n;
+                        n++;
+                    
+                }
+
+                if(i == sum)
+                
+
+                    Console.WriteLine("{0}" , i);
+
+            }
+
+
+
+
+
+
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+        }
+    }
+}
